317c049d475b23bad32250762f3619e550e2f67e
Seyed Mostafa Kia <m.kia83@gmail.com>
2017-08-14 09:52:55 +0200

    Rearrenging the files.

--- experiments/Simulation/simulation_demo.py
+++ experiments/Simulation/simulation_demo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2.7
+
 import scipy as SP
 import core.gp.gp_base as gp_base
 import core.gp.gp_kronprod as gp_kronprod
@@ -13,19 +15,24 @@ import core.likelihood.likelihood_base as lik
 from scipy.io import loadmat
 from sklearn.metrics import mean_squared_error
 from core.util.utilities import compute_r2
-from core.util.normod import extreme_value_prob, normative_prob_map, threshold_NPM
+from core.util.normod import extreme_value_prob, normative_prob_map
 from sklearn.metrics import roc_auc_score
-import seaborn as sns
 import pickle
-import matplotlib.pylab as plt
 
 if __name__ == "__main__":
     
     method = 'all'
-    
+    data_path = '/home/mrstats/seykia/Data/Pygp/Simulated_Data/'
+    save_path = '/home/mrstats/seykia/Results/Pygp/Simulated_Data/'
+    dataset = 'big'
     simulation_num = 10
     n_samples = 150
-    n_tasks = 100
+    if dataset == 'small':
+        n_tasks = 4
+        perc = 0.5
+    elif dataset == 'big':
+        n_tasks = 100
+        perc = 0.1
     
     results_base = dict({'Y_pred':np.zeros([simulation_num, n_samples,n_tasks]),
                          'Y_pred_cov':np.zeros([simulation_num, n_samples,n_tasks]),
@@ -66,9 +73,9 @@ if __name__ == "__main__":
                          'mse': np.zeros([simulation_num,]) 
                         })
                         
-    for s in range (simulation_num):
+    for s in range(simulation_num):
         
-        matContent = loadmat('./demo/dataset' + str(s+1) +'.mat')
+        matContent = loadmat(data_path + dataset +'/dataset' + str(s+1) +'.mat')
         
         
         
@@ -81,7 +88,7 @@ if __name__ == "__main__":
         n_samples = X_train.shape[0]
         n_tasks = Y_train.shape[1]
         n_dimensions = X_train.shape[1]
-        n_latent = 10
+        n_latent = 1
         
         # Normalization
         X_scaler = StandardScaler()
@@ -114,12 +121,12 @@ if __name__ == "__main__":
             
             results_base['NPM'][s,:,:] = normative_prob_map(Y_test, results_base['Y_pred'][s,:,:], 
                                             results_base['Y_pred_cov'][s,:,:], results_base['s_n2'][s,:])
-            results_base['abnormal_prob'][s,:] = extreme_value_prob(results_base['NPM'][s,:,:], 0.01)
+            results_base['abnormal_prob'][s,:] = extreme_value_prob(results_base['NPM'][s,:,:], perc)
             results_base['auc'][s] = roc_auc_score(labels,results_base['abnormal_prob'][s,:])
                
             results_base['r2'][s] = np.mean(compute_r2(Y_test, results_base['Y_pred'][s,:,:]))
             results_base['mse'][s] = mean_squared_error(Y_test,results_base['Y_pred'][s,:,:], multioutput='uniform_average')
-            print 'Dataset: %i, GP_Base: R2 = %f, MSE = %f' %(s+1, results_base['r2'][s], results_base['mse'][s])
+            print 'Dataset: %i, GP_Base: R2 = %f, MSE = %f, AUC = %f' %(s+1, results_base['r2'][s], results_base['mse'][s], results_base['auc'][s])
             
         ################################# Pooling Approach ########################
         if (method == 'pool' or  method == 'all'): 
@@ -143,12 +150,12 @@ if __name__ == "__main__":
             
             results_pool['NPM'][s,:,:] = normative_prob_map(Y_test, results_pool['Y_pred'][s,:,:], 
                                           results_pool['Y_pred_cov'][s,:,:], results_pool['s_n2'][s,:])
-            results_pool['abnormal_prob'][s,:] = extreme_value_prob(results_pool['NPM'][s,:,:], 0.01)
+            results_pool['abnormal_prob'][s,:] = extreme_value_prob(results_pool['NPM'][s,:,:], perc)
             results_pool['auc'][s] = roc_auc_score(labels, results_pool['abnormal_prob'][s,:])
                 
             results_pool['r2'][s] = np.mean(compute_r2(Y_test, results_pool['Y_pred'][s,:,:]))
             results_pool['mse'][s] = mean_squared_error(Y_test, results_pool['Y_pred'][s,:,:], multioutput='uniform_average')
-            print 'Dataset: %i, GP_Pool: R2 = %f, MSE = %f' %(s+1,results_pool['r2'][s], results_pool['mse'][s])
+            print 'Dataset: %i, GP_Pool: R2 = %f, MSE = %f, AUC = %f' %(s+1,results_pool['r2'][s], results_pool['mse'][s], results_pool['auc'][s])
         
         ################################# Kronprod Approach ########################
         if (method == 'prod' or  method == 'all'):              
@@ -173,12 +180,12 @@ if __name__ == "__main__":
            
             results_prod['NPM'][s,:,:] = normative_prob_map(Y_test, results_prod['Y_pred'][s,:,:], 
                                                     results_prod['Y_pred_cov'][s,:,:], results_prod['s_n2'][s,:])
-            results_prod['abnormal_prob'][s,:] = extreme_value_prob(results_prod['NPM'][s,:,:], 0.01)
+            results_prod['abnormal_prob'][s,:] = extreme_value_prob(results_prod['NPM'][s,:,:], perc)
             results_prod['auc'][s] = roc_auc_score(labels, results_prod['abnormal_prob'][s,:])    
             
             results_prod['r2'][s] = np.mean(compute_r2(Y_test, results_prod['Y_pred'][s,:,:]))
             results_prod['mse'][s] = mean_squared_error(Y_test, results_prod['Y_pred'][s,:,:], multioutput='uniform_average')
-            print 'Dataset: %i, GP_Kronprod: R2 = %f, MSE = %f' %(s+1,results_prod['r2'][s], results_prod['mse'][s]) 
+            print 'Dataset: %i, GP_Kronprod: R2 = %f, MSE = %f, AUC = %f' %(s+1,results_prod['r2'][s], results_prod['mse'][s], results_prod['auc'][s]) 
             
         ################################# Kronsum Approach ########################
         if (method == 'sum' or  method == 'all'):     
@@ -197,7 +204,7 @@ if __name__ == "__main__":
             covar_s.X = hyperparams['X_s']
             covar_c.X = hyperparams['X_c']
             X_o = SP.zeros((Y_train.shape[0], n_dimensions))
-            
+            0.01
             gp = gp_kronsum.KronSumGP(covar_c = covar_c, covar_r = covar_r, covar_s = covar_s, 
                                       covar_o = covar_o)
             gp.setData(Y = Y_train, X_r = X_train, X_o = X_o)  
@@ -215,43 +222,23 @@ if __name__ == "__main__":
         
             results_sum['NPM'][s,:,:] = normative_prob_map(Y_test, results_sum['Y_pred'][s,:,:], 
                                          results_sum['Y_pred_cov'][s,:,:], results_sum['s_n2'][s,:])
-            results_sum['abnormal_prob'][s,:] = extreme_value_prob(results_sum['NPM'][s,:,:], 0.01)
+            results_sum['abnormal_prob'][s,:] = extreme_value_prob(results_sum['NPM'][s,:,:], perc)
             results_sum['auc'][s] = roc_auc_score(labels, results_sum['abnormal_prob'][s,:])    
             
             results_sum['r2'][s] = np.mean(compute_r2(Y_test, results_sum['Y_pred'][s,:,:]))
             results_sum['mse'][s] = mean_squared_error(Y_test, results_sum['Y_pred'][s,:,:], multioutput='uniform_average')
-            print 'Dataset: %i, GP_Kronsum: R2 = %f, MSE = %f' %(s+1,results_sum['r2'][s], results_sum['mse'][s]) 
+            print 'Dataset: %i, GP_Kronsum: R2 = %f, MSE = %f, AUC = %f' %(s+1,results_sum['r2'][s], results_sum['mse'][s], results_sum['auc'][s]) 
                              
     ########################################## Saving the results#############
-    with open('Results_base.pkl', 'wb') as f:
-        pickle.dump(results_base, f)
+        with open(save_path + dataset + '_Results_base.pkl', 'wb') as f:
+            pickle.dump(results_base, f)
+            
+        with open(save_path + dataset + '_Results_pool.pkl', 'wb') as f:
+            pickle.dump(results_pool, f)
         
-    with open('Results_pool.pkl', 'wb') as f:
-        pickle.dump(results_pool, f)
-    
-    with open('Results_prod.pkl', 'wb') as f:
-                pickle.dump(results_prod, f)
-                                 
-    with open('Results_sum.pkl', 'wb') as f:
-            pickle.dump(results_sum, f)  
+        with open(save_path + dataset + '_Results_prod.pkl', 'wb') as f:
+                    pickle.dump(results_prod, f)
+                                     
+        with open(save_path + dataset + '_Results_sum.pkl', 'wb') as f:
+                pickle.dump(results_sum, f)  
                              
-    ######################################### Ploting ########################
-#    import pickle
-#    with open('Results_base.pkl', 'rb') as f:
-#        Results_base = pickle.load(f)
-#    with open('Results_pool.pkl', 'rb') as f:
-#        Results_pool = pickle.load(f)
-#    with open('Results_prod.pkl', 'rb') as f:
-#        Results_prod = pickle.load(f)
-#    with open('Results_sum.pkl', 'rb') as f:
-#        Results_sum = pickle.load(f)
-#    abn_samples =  np.squeeze(np.array(np.nonzero (labels)))
-#    ax = []
-#    fig = plt.figure()
-#    for i in range(8):
-#        sns.heatmap(np.reshape(threshold_NPM(Results_base['NPM_base'][abn_samples[i],:], 0.05),[10,10]), ax = fig.add_subplot(4,8,1+i))
-#        sns.heatmap(np.reshape(threshold_NPM(Results_pool['NPM_pool'][abn_samples[i],:], 0.05),[10,10]), ax = fig.add_subplot(4,8,9+i))
-#        sns.heatmap(np.reshape(threshold_NPM(Results_prod['NPM_prod'][abn_samples[i],:], 0.05),[10,10]), ax = fig.add_subplot(4,8,17+i))
-#        sns.heatmap(np.reshape(threshold_NPM(Results_sum['NPM_sum'][abn_samples[i],:], 0.05),[10,10]), ax = fig.add_subplot(4,8,25+i))
-#    
-#    plt.show()
